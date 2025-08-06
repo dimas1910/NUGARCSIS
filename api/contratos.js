b9195d56f1e5fa8b4bf0004141964875a4ae9e89
dimas1910 <dimasadonascimento@gmail.com>
2025-08-06 00:47:29 -0300

    Update contratos.js

--- api/contratos.js
+++ api/contratos.js
@@ -1,5 +1,6 @@
 export default async function handler(req, res) {
   const pagina = req.query.pagina || 1;
+  const tamanhoPagina = req.query.tamanhoPagina || 10;
 
   const url = `https://dadosabertos.compras.gov.br/modulo-contratos/1_consultarContratos?pagina=${pagina}&tamanhoPagina=10&codigoUnidadeGestora=100001&dataVigenciaInicialMin=2025-01-01&dataVigenciaInicialMax=2025-12-31`;
 
